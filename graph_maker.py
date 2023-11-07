f9554e1fd8e576fe8c1cafc7d30ca7ef8a63ead6
Elisa Fabbri <elisa.fabbri40@studio.unibo.it>
2023-11-07 16:06:21 +0100

    work done together

--- graph_maker.py
+++ graph_maker.py
@@ -23,15 +23,80 @@ for item in data:
         # Add edges from source_id to the cited articles
         G.add_edge(source_id, target_id)
 
-# Now you can perform various operations on the graph 'G'
-# For example, you can get basic information about the graph:
-print(f"Number of nodes: {len(G.nodes)}")
-print(f"Number of edges: {len(G.edges)}")
 
-pos = nx.spring_layout(G, seed=42)  # You can choose a different layout if needed
+# Number of nodes and edges
+num_nodes = len(G.nodes)
+num_edges = len(G.edges)
+print(f"Number of nodes: {num_nodes}")
+print(f"Number of edges: {num_edges}")
 
-plt.figure(figsize=(10, 10))
-nx.draw(G, pos, with_labels=False, node_size=1, node_color='blue', font_size=6, width=0.1)
-plt.title("Citation Graph")
-plt.axis('off')
+# Degree distribution
+in_degrees = dict(G.in_degree())
+out_degrees = dict(G.out_degree())
+
+# Connected components
+weakly_connected_components = list(nx.weakly_connected_components(G))
+
+
+# Centrality measures (e.g., in-degree and out-degree centrality)
+in_degree_centrality = nx.in_degree_centrality(G)
+out_degree_centrality = nx.out_degree_centrality(G)
+
+# Network density
+density = nx.density(G)
+
+# Clustering coefficients
+clustering_coefficients = nx.clustering(G)
+
+# Transitivity
+transitivity = nx.transitivity(G)
+
+# Reciprocity
+reciprocity = nx.reciprocity(G)
+
+
+# Central node identification (e.g., nodes with high in-degree centrality)
+top_nodes_by_in_degree = sorted(in_degree_centrality, key=in_degree_centrality.get, reverse=True)[:5]
+
+# Assortativity
+assortativity = nx.degree_assortativity_coefficient(G)
+
+
+
+# Degree distribution
+in_degrees = dict(G.in_degree())
+out_degrees = dict(G.out_degree())
+
+# Plot in-degree distribution
+plt.figure(figsize=(8, 6))
+plt.hist(list(in_degrees.values()), bins=20, alpha=0.5, color='b')
+plt.title('In-Degree Distribution')
+plt.xlabel('In-Degree')
+plt.ylabel('Count')
+plt.show()
+
+# Plot out-degree distribution
+plt.figure(figsize=(8, 6))
+plt.hist(list(out_degrees.values()), bins=20, alpha=0.5, color='r')
+plt.title('Out-Degree Distribution')
+plt.xlabel('Out-Degree')
+plt.ylabel('Count')
+plt.show()
+
+# Connected components
+strongly_connected_components = list(nx.strongly_connected_components(G))
+weakly_connected_components = list(nx.weakly_connected_components(G))
+
+# Plot the number of strongly connected components
+plt.figure(figsize=(8, 6))
+plt.bar(['Strongly Connected'], [len(strongly_connected_components)], color='g')
+plt.title('Number of Strongly Connected Components')
+plt.ylabel('Count')
+plt.show()
+
+# Plot the number of weakly connected components
+plt.figure(figsize=(8, 6))
+plt.bar(['Weakly Connected'], [len(weakly_connected_components)], color='y')
+plt.title('Number of Weakly Connected Components')
+plt.ylabel('Count')
 plt.show()
