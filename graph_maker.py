72c288a6399db5a290d4493654c9a84dc59c1570
Elisa Fabbri <elisa.fabbri40@studio.unibo.it>
2023-11-07 13:54:01 +0100

    first working version of graph maker and visualizer, may be slow

--- graph_maker.py
+++ graph_maker.py
@@ -4,23 +4,34 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import json
 
-# Read the JSON data line by line
 with open('pubmed_data.json', 'r') as file:
     data = [json.loads(line) for line in file]
+print(type(data))
 
-# Create a DataFrame
-df = pd.DataFrame(data)
+# Create a directed graph
+G = nx.DiGraph()
 
-# Now you can proceed with the rest of your code
-print(df.columns)
+# Add nodes and edges
+for item in data:
+    source_id = item["Id"]
+    cited_articles = item["Cited_Articles"]
 
+    # Add nodes (if not already added)
+    G.add_node(source_id)
 
-# Create a NetworkX graph from the DataFrame
-G = nx.from_pandas_edgelist(df, 'Id', 'Cited_Articles')
+    for target_id in cited_articles:
+        # Add edges from source_id to the cited articles
+        G.add_edge(source_id, target_id)
 
 # Now you can perform various operations on the graph 'G'
 # For example, you can get basic information about the graph:
 print(f"Number of nodes: {len(G.nodes)}")
 print(f"Number of edges: {len(G.edges)}")
 
+pos = nx.spring_layout(G, seed=42)  # You can choose a different layout if needed
 
+plt.figure(figsize=(10, 10))
+nx.draw(G, pos, with_labels=False, node_size=1, node_color='blue', font_size=6, width=0.1)
+plt.title("Citation Graph")
+plt.axis('off')
+plt.show()
