d022f770c9a8743992fdbeac46a526fb2b0a7bb9
Giovanni Faglioni <giova@nabla2.it>
2023-11-12 21:02:01 +0100

    working fetcher and some data

--- d_fetcher.py
+++ d_fetcher.py
@@ -1,152 +1,144 @@
 import requests
 import xml.etree.ElementTree as ET
-from tqdm import tqdm
 import json
 import pandas as pd
+from concurrent.futures import ThreadPoolExecutor
+from tqdm import tqdm
+import time
 
-def get_pubmed_ids(term):
+def split_ids(id_list, cluster_size=100):
+    return [id_list[i:i + cluster_size] for i in range(0, len(id_list), cluster_size)]
+
+def read_ids_from_csv(file_path):
     """
-    Fetch PubMed IDs for a specific term.
+    Read PubMed IDs from a CSV file.
 
     Args:
-        term (str): The search term.
-        retstart (int): The index of the first record to retrieve.
-        retmax (int, optional): The maximum number of records to retrieve. Default is 50.
+        file_path (str): Path to the CSV file containing PubMed IDs.
 
     Returns:
         list: List of PubMed IDs.
     """
-    all_ids = []
-    for year in range(2015, 2016):
-        for i in tqdm(range(6)):
-            min_month = i * 2 + 1
-            max_month = i * 2 + 2
-            if max_month > 12:
-                max_month = 12
-            min_date = f"{year}/{min_month:02d}"
-            max_date = f"{year}/{max_month:02d}"
+    column_label = 'PubMed_ID'  
+    df = pd.read_csv(file_path, names=[column_label], header=0)
+    return df[column_label][:1000].tolist()  # Adjust 'PubMed_ID' based on your CSV column name
 
-            
-            esearch_url = f"https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=pubmed&term={term}&mindate={min_date}&maxdate={max_date}&format=json"
-            response = requests.get(esearch_url)
-            retmax = response.json()["esearchresult"]["count"]
-            esearch_url_1 = f"https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=pubmed&term={term}&mindate={min_date}&maxdate={max_date}&retstart={0}&retmax={retmax}&format=json"
-            response_1 = requests.get(esearch_url_1)
-            ids = response_1.json()["esearchresult"]["idlist"]
-            all_ids.extend(ids)
-    print(len(all_ids))
-    return all_ids
-
-def split_ids(id_list):
-    return [id_list[i:i + 10] for i in range(0, len(id_list), 10)]
-
-
-def fetch_pubmed(ids):
+def parse_article(article):
     """
-    Fetch and Link PubMed data for a list of PubMed IDs.
+    Parse individual PubMed article data from XML.
+
+    Args:
+        article (xml.etree.ElementTree.Element): XML element for a single article.
+
+    Returns:
+        dict: Parsed article data.
+    """
+    # Extract PubMed ID
+    pubmed_id = article.find(".//MedlineCitation/PMID").text
+
+    # Extract publication date
+    pub_date_elem = article.find(".//PubDate")
+    year = pub_date_elem.findtext(".//Year") or "Unknown"
+    month = pub_date_elem.findtext(".//Month") or "Unknown"
+    day = pub_date_elem.findtext(".//Day") or "Unknown"
+    pub_date = f"{year}-{month}-{day}"
+
+    # Extract authors
+    authors_elem = article.find(".//AuthorList")
+    authors = [
+        f"{author.findtext('.//LastName') or author.findtext('.//CollectiveName') or 'pd'} {author.findtext('.//ForeName') or author.findtext('.//CollectiveName') or 'pd'}"
+        for author in authors_elem.findall(".//Author")
+    ] if authors_elem is not None else ["Unknown"]
+
+    # Extract MeSH terms with MajorTopicYN distinction
+    mesh_elems = article.findall(".//MeshHeadingList/MeshHeading")
+    mesh_terms = []
+    for mesh in mesh_elems:
+        descriptor = mesh.find('.//DescriptorName')
+        descriptor_name = descriptor.text
+        descriptor_major_topic = descriptor.get('MajorTopicYN') == 'Y'
+
+        # Process QualifierNames if any
+        qualifiers = mesh.findall('.//QualifierName')
+        for qual in qualifiers:
+            qual_name = qual.text
+            qual_major_topic = qual.get('MajorTopicYN') == 'Y'
+            mesh_terms.append({
+                'DescriptorName': descriptor_name,
+                'DescriptorMajorTopic': descriptor_major_topic,
+                'QualifierName': qual_name,
+                'QualifierMajorTopic': qual_major_topic
+            })
+
+        # Include DescriptorName if there are no QualifierNames
+        if not qualifiers:
+            mesh_terms.append({
+                'DescriptorName': descriptor_name,
+                'DescriptorMajorTopic': descriptor_major_topic,
+                'QualifierName': None,
+                'QualifierMajorTopic': None
+            })
+
+    return {
+        "Id": pubmed_id,
+        "Dates": pub_date,
+        "Authors": authors,
+        "MeSH_Terms": mesh_terms
+    }
+def fetch_data_for_ids(ids, retries=6, delay=1):
+    """
+    Fetch and parse PubMed data for a list of PubMed IDs.
 
     Args:
         ids (list): List of PubMed IDs.
+        retries (int): Number of retries for the request.
+        delay (int): Delay between retries in seconds.
 
     Returns:
-        pd.DataFrame: DataFrame containing PubMed data.
+        list: List of dictionaries containing parsed article data.
     """
-    id_string = ",".join(ids)
-    efetch_url = f"https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=pubmed&id={id_string}&rettype=abstract"
-    response = requests.get(efetch_url)
-    xml_data = response.text
-    root = ET.fromstring(xml_data)
-    articles_dates = []
-    articles_authors = []
-    articles_kwords = []
-    for article in root.findall(".//PubmedArticle"):
-        # Extract publication date
-        pub_date_elem = article.find(".//PubDate")
-        year = pub_date_elem.findtext(".//Year") or "Unknown"
-        month = pub_date_elem.findtext(".//Month") or "Unknown"
-        day = pub_date_elem.findtext(".//Day") or "Unknown"
-        pub_date = f"{year}-{month}-{day}"
-        articles_dates.append(pub_date)
-
-        # Extract authors
-        authors_elem = article.find(".//AuthorList")
-        if authors_elem is not None:
-            authors = [
-                f"{author.findtext('.//LastName') or 'Unknown'} {author.findtext('.//ForeName') or 'Unknown'}"
-                for author in authors_elem.findall(".//Author")
-            ]
+    for attempt in range(retries):
+        id_string = ",".join(str(id) for id in ids)
+        efetch_url = f"https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=pubmed&id={id_string}&rettype=abstract"
+        
+        response = requests.get(efetch_url)
+        
+        if response.status_code == 200:
+            root = ET.fromstring(response.text)
+            return [parse_article(article) for article in root.findall(".//PubmedArticle")]
+        elif response.status_code == 429:
+            print(f"Rate limit hit. Retrying after {delay} seconds...")
+            time.sleep(delay)
+            delay *= 2  # Exponential backoff
         else:
-            authors = ["Unknown"]
-        articles_authors.append(authors)
+            print(f"Failed to fetch data. Status Code: {response.status_code}")
+            break
 
-        # Extract keywords
-        keyword_elems = article.findall(".//KeywordList/Keyword")
-        article_keywords = [keyword.text for keyword in keyword_elems]
-        articles_kwords.append(article_keywords)
+    return []
 
-    articles_cited = []
-    articles_cited_in = []
-    string_for_smart_linking = ""
-    for i in ids:
-        string_for_smart_linking += f"&id={i}"
-    elink_url = (
-        f"https://eutils.ncbi.nlm.nih.gov/entrez/eutils/elink.fcgi?dbfrom=pubmed&linkname=pubmed_pubmed_refs"
-        + string_for_smart_linking
-    )
-    elink_url_cited_in = (
-        f"https://eutils.ncbi.nlm.nih.gov/entrez/eutils/elink.fcgi?dbfrom=pubmed&linkname=pubmed_pubmed_citedin"
-        + string_for_smart_linking
-    )
+def fetch_pubmed(ids_list):
+    """
+    Fetch PubMed data sequentially for multiple lists of IDs.
 
-    response_l = requests.get(elink_url)
-    xml_data_l = response_l.text
+    Args:
+        ids_list (list of list): List containing sublists of PubMed IDs.
 
-    response_cited_in = requests.get(elink_url_cited_in)
-    xml_data_cited_in = response_cited_in.text
+    Returns:
+        pd.DataFrame: DataFrame containing combined PubMed data.
+    """
+    all_results = []
 
-    root_l = ET.fromstring(xml_data_l)
-    root_cited_in = ET.fromstring(xml_data_cited_in)
+    for ids in tqdm(ids_list):
+        fetched_data = fetch_data_for_ids(ids)
+        all_results.extend(fetched_data)
 
-    for link in root_l.findall(".//LinkSet"):
-        link_elems = link.findall(".//LinkSetDb/Link/Id")
-        cited_arts = [art.text for art in link_elems]
-        articles_cited.append(cited_arts)
-
-    for link in root_cited_in.findall(".//LinkSet"):
-        link_elems = link.findall(".//LinkSetDb/Link/Id")
-        cited_arts = [art.text for art in link_elems]
-        articles_cited_in.append(cited_arts)
-    print(len(ids))
-    print(len(articles_dates))
-    print(len(articles_authors))
-    print(len(articles_kwords))
-    print(len(articles_cited))
-    print(len(articles_cited_in))
-
-    data = pd.DataFrame(
-        {
-            "Id": ids,
-            "Dates": articles_dates,
-            "Authors": articles_authors,
-            "Keywords": articles_kwords,
-            "Cited_Articles": articles_cited,
-            "Articles_Cited_In": articles_cited_in,
-        }
-    )
-
-    return data
-
+    return pd.DataFrame(all_results)
 
 # Usage
-
-data = []
-ids = get_pubmed_ids("alzheimer")
+file_path = 'ids.csv'  
+ids = read_ids_from_csv(file_path)
 splitted_ids = split_ids(ids)
+print(type(splitted_ids))
+df = fetch_pubmed(splitted_ids)
+df.to_json("pubmed_data.json", orient="records", lines=True)
 
-for id_list in tqdm(splitted_ids):
-    data.append(fetch_pubmed(id_list))
-
-unique_df = pd.concat(data)
-print(unique_df)
-#unique_df.to_json("pubmed_data.json", orient="records", lines=True)
-#unique_df.to_hdf("pubmed_data.h5", key="pubmed_data", mode="w")
