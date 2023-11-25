a18b24f8039285d3fc8bdece70c9d4b9dbf4f42f
Elisa Fabbri <elisa.fabbri40@studio.unibo.it>
2023-11-25 17:30:46 +0100

    riorganizzazione repo

--- PARALLEL/go.sh
+++
@@ -1,5 +0,0 @@
-python algo1.py &
-
-python algo2.py &
-
-python algo3.py &
---
+++ go.sh
@@ -0,0 +1,5 @@
+python clustering_coefficient.py &
+
+python clustering_coefficient_unw.py &
+
+python average_shortest_path.py &
